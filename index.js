e87c73d14cef1fa43d2c8ac194a1e9184361ff97
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2021-11-13 13:37:37 -0600

    added moment js to datetime fields

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function () {
+
     var myConnector = tableau.makeConnector();
 
     myConnector.init = function (initCallback) {
@@ -111,19 +112,20 @@
                     console.log(response)
 
                     if (resp.length > 0) {
+                        var dateFormat = "Y-MM-DD HH:mm:ss";
                         // Iterate over the JSON object
                         for (var i = 0, len = response.length; i < len; i++) {
                             tableData.push({
                                 "id": (response[i].display_id).toString(),
                                 "source": response[i].source,
                                 "priority_name": response[i].priority_name,
-                                "created_at": response[i].created_at,
-                                "updated_at": response[i].updated_at,
+                                "created_at": moment(response[i].created_at).format(dateFormat),
+                                "updated_at": moment(response[i].updated_at).format(dateFormat),
                                 "first_response_escalated": response[i].fr_escalated,
                                 "documentation_required": (response[i].custom_field.cf_documentation_required_876066 == null ? 'Empty' : response[i].custom_field.cf_documentation_required_876066),
                                 "ticket_type": (response[i].ticket_type == null ? 'Empty' : response[i].ticket_type),
                                 "group_id": (response[i].group_id == null ? 123 : response[i].group_id).toString(),
-                                "due_by": response[i].due_by,
+                                "due_by": moment(response[i].due_by).format(dateFormat),
                                 "requester_name": response[i].requester_name,
                                 "requester_id": (response[i].requester_id).toString(),
                                 "subject": response[i].subject,
